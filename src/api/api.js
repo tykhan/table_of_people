078e91df207bd77315b3ef718d694eec7ab64914
tykhan <tykhanvasyl@gmail.com>
2020-12-25 21:08:27 +0200

    API implemented

---
+++ src/api/api.js
@@ -0,0 +1,43 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+const request = (endpoint, options) => {
+  return fetch(`${BASE_URL}${endpoint}`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+}
+
+const postMethod = (url, data) => {
+  return request(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data)
+  });
+}
+
+const deleteMethod = (url) => {
+  return request(url, { method: 'DELETE' });
+}
+
+const putMethod = (url, data) => {
+  return request(url, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data)
+  });
+}
+
+export const getUsers = () => request('/users');
+export const getUserPosts = (userId) => request(`/posts?userId=${userId}`);
+export const getPostComments = (postId) => request(`/comments?${postId}`);
+export const editPost = (postId) => putMethod(`/posts/${postId}`);
+export const addNewPost = (data) => postMethod('/posts');
+export const deletePost = (postId) => deleteMethod(`/posts/${postId}`);
